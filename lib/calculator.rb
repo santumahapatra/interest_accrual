5bd3322944c9657d2e3129aee92d781bd077beb5
Santu Mahapatra <santu.m@multunus.com>
2015-02-18 11:54:45 +0530

    Refactoring

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,15 +1,27 @@
 class Calculator
-  def daily_interest(principal, rate_of_interest)
-    (principal * rate_of_interest).round(2)
+  attr_accessor :start_loan_amount, :rate_of_interest, :disbursement_date
+
+  def initialize(start_loan_amount, rate_of_interest, disbursement_date)
+    @start_loan_amount = start_loan_amount
+    @rate_of_interest = rate_of_interest
+    @disbursement_date = disbursement_date
   end
 
-  def interest_accrued(start_date, end_date, start_date_amount, rate_of_interest)
+  def daily_interest(principal)
+    (principal * @rate_of_interest).round(2)
+  end
+
+  def interest_accrued(start_date, end_date, start_date_amount)
     total_interest = 0
     principal = start_date_amount
     start_date.upto(end_date) do
-      total_interest += daily_interest(principal, rate_of_interest)
+      interest = daily_interest(principal)
+      total_interest += interest
+      principal += interest
     end
 
     total_interest
   end
+
+  
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,16 +2,12 @@ require 'calculator.rb'
 require 'date'
 
 describe Calculator do
-  let(:principal) {10000.00}
-  let(:rate_of_interest) { (0.15/360) }
-  let(:underwriter) { Calculator.new }
-  let(:loan) { { amount: 1000, date: Date.new(2015, 1, 1) } }
-
+  let(:underwriter) { Calculator.new(10000, (0.15/360), Date.new(2015, 1, 1)) }
 
   describe '#daily_interest' do
     it 'calculates the daily interest' do
-      expect(underwriter.daily_interest(principal, rate_of_interest)).to be(4.17)
-      expect(underwriter.daily_interest(principal, rate_of_interest)).not_to be(4.16)
+      expect(underwriter.daily_interest(10000)).to be(4.17)
+      expect(underwriter.daily_interest(10000)).not_to be(4.16)
     end
   end
 
@@ -19,7 +15,7 @@ describe Calculator do
     it 'calculate the interest accrued in the period' do
       start_date = Date.new(2015, 1, 1)
       end_date = Date.new(2015, 1, 2)
-      expect(underwriter.interest_accrued(start_date, end_date, principal, rate_of_interest)).to be(8.34)
+      expect(underwriter.interest_accrued(start_date, end_date, 10000)).to be(8.34)
     end
   end
 end
